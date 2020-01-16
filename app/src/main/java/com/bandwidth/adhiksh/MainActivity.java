a71c9198f7b771e101642437044a4acef8aaf74b
isouravrex <isouravrex@gmail.com>
2020-01-16 22:06:48 +0530

    First commit

---
+++ app/src/main/java/com/bandwidth/adhiksh/MainActivity.java
@@ -0,0 +1,41 @@
+package com.bandwidth.adhiksh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton login;
+    TextView signUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUIViews();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NOCForm.class));
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+            }
+        });
+    }
+
+    private void setUIViews(){
+        login = findViewById(R.id.btn_login);
+        signUp = findViewById(R.id.tv_signup);
+    }
+}
