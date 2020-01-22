03f259f588d249683df85885886613e27a855edb
isouravrex <isouravrex@gmail.com>
2020-01-22 15:35:41 +0530

    First commit

--- app/src/main/java/com/bandwidth/adhiksh/FormSubmission.java
+++ app/src/main/java/com/bandwidth/adhiksh/FormSubmission.java
@@ -2,13 +2,32 @@ package com.bandwidth.adhiksh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class FormSubmission extends AppCompatActivity {
+    MaterialButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_submission);
+        setUIViews();
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FormSubmission.this, CitizenHomepage.class));
+            }
+        });
+
+    }
+
+    private void setUIViews(){
+
+        back = findViewById(R.id.btn_back);
     }
 }
