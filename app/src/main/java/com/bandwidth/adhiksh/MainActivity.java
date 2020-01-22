e77c44dd08c0675144ea2a6a1a4ea432d6d3224c
isouravrex <isouravrex@gmail.com>
2020-01-23 01:34:26 +0530

    First commit

--- app/src/main/java/com/bandwidth/adhiksh/MainActivity.java
+++ app/src/main/java/com/bandwidth/adhiksh/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +24,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUIViews();
+        RadioButton radioButtonCitizen = (RadioButton) findViewById(R.id.radioCitizen);
+        final RadioButton radioButtonAuthority = (RadioButton) findViewById(R.id.radioAuthority);// initiate a radio button
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CitizenHomepage.class));
-            }
-        });
+
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(radioButtonAuthority.isChecked()) {
+                        startActivity(new Intent(MainActivity.this, AuthorityHome.class));
+                    }
+                    else
+                    {
+                        startActivity(new Intent(MainActivity.this, CitizenHomepage.class));
+                    }
+
+                }
+            });
+
+
+
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,4 +57,5 @@ public class MainActivity extends AppCompatActivity {
         login = findViewById(R.id.btn_login);
         signUp = findViewById(R.id.tv_signup);
     }
+
 }
