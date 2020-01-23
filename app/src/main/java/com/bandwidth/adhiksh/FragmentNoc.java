b9e3417bdde8558343d7ba8e69f853e37d430db6
isouravrex <isouravrex@gmail.com>
2020-01-23 16:38:14 +0530

    23/01Commit

---
+++ app/src/main/java/com/bandwidth/adhiksh/FragmentNoc.java
@@ -0,0 +1,31 @@
+package com.bandwidth.adhiksh;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentNoc extends Fragment {
+
+
+    public FragmentNoc() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_noc, container, false);
+    }
+
+}
