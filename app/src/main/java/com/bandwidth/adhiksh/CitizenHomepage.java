48522e674575fb7a1ccdf259a876b8a7675f08ef
isouravrex <isouravrex@gmail.com>
2020-01-29 17:24:59 +0530

    29/01 commitdone

--- app/src/main/java/com/bandwidth/adhiksh/CitizenHomepage.java
+++ app/src/main/java/com/bandwidth/adhiksh/CitizenHomepage.java
@@ -1,21 +1,49 @@
 package com.bandwidth.adhiksh;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class CitizenHomepage extends AppCompatActivity {
 
     LinearLayout registerFIR,requestNOC,registerComplaint,requestAppointment;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawer;
+    ImageView icon;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citizen_homepage);
         setUIViews();
+
+        drawer=findViewById(R.id.drawer_layout);
+        toolbar=findViewById(R.id.toolbar);
+
+        toggle = new ActionBarDrawerToggle(CitizenHomepage.this,drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        drawer.closeDrawers();
+
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+
 
         registerFIR.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,5 +79,17 @@ public class CitizenHomepage extends AppCompatActivity {
         registerComplaint = findViewById(R.id.registerComplaint);
         requestNOC = findViewById(R.id.requestNOC);
         requestAppointment = findViewById(R.id.requestAppointment);
+        icon = findViewById(R.id.btn_drawer);
     }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(Gravity.LEFT))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+
+
+
 }
